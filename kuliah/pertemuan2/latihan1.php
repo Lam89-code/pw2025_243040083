56227cadbffe3c3c1f7e4b4cb97b0f6693e9213a
Lam89-code <aaallaaamm03@gmail.com>
2025-02-12 11:37:07 +0700

    Update Terbaru

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h1><?php 
+    echo"KeOyo Malam Malam, Halo Aku Alam";
+    ?></h1>
+
+    <h4>Nama Saya : <?php echo "Alam"; ?></h4>
+</body>
+</html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php  
+
+$nama ="Muhamad Nur Salam";
+$email = "aaallaaamm03@gmail.com";
+$pekerjaan = "Mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "Universitas Pasundan";
+
+echo "<h1>$nama</h1>";
+echo "<p>$email/p>";
+echo "Hallo, nama saya $nama, saya adalah $pekerjaan $jurusan $universitas";
+
+?>
